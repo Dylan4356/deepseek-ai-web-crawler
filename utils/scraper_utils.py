edda4eb9effbf3c7f6bccc0a8c04693c7ad5fbf7
Dylan4356 <125047817+Dylan4356@users.noreply.github.com>
2025-08-12 11:33:21 -0400

    Update for deprecated features

--- utils/scraper_utils.py
+++ utils/scraper_utils.py
@@ -8,6 +8,7 @@ from crawl4ai import (
     CacheMode,
     CrawlerRunConfig,
     LLMExtractionStrategy,
+    LLMConfig
 )
 
 from models.venue import Venue
@@ -38,7 +39,7 @@ def get_llm_strategy() -> LLMExtractionStrategy:
     """
     # https://docs.crawl4ai.com/api/strategies/#llmextractionstrategy
     return LLMExtractionStrategy(
-        provider="groq/deepseek-r1-distill-llama-70b",  # Name of the LLM provider
+        llm_config=LLMConfig(provider="groq/deepseek-r1-distill-llama-70b"),  # Name of the LLM provider
         api_token=os.getenv("GROQ_API_KEY"),  # API token for authentication
         schema=Venue.model_json_schema(),  # JSON schema of the data model
         extraction_type="schema",  # Type of extraction to perform
